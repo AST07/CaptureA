d5b52b36e97be27244764333201138518165e458
Aditya Singh Tejas <69631629+AST07@users.noreply.github.com>
2020-12-07 17:16:57 +0530

    Add files via upload

---
+++ CaptureA.py
@@ -0,0 +1,147 @@
+'''
+CaptureA Copyright (c) 2020 Aditya Singh Tejas
+05-12-2020
+Update 1: 07-12-2020 (+ Remember the last location entered)
+'''
+
+from tkinter import *
+from tkinter import ttk,filedialog,messagebox
+from datetime import datetime
+import os
+from PIL import Image,ImageTk,ImageGrab
+from threading import Thread
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def launch_button_change(opt):
+	global launch_button,cap,inst_label
+	options=[['Launch','RoyalBlue2','launch',''],['Close','red','lambda:on_close_cap()',inst]]
+	launch_button.configure(text=options[opt][0],bg=options[opt][1],command=eval(options[opt][2]))
+	inst_label.config(text=options[opt][3])
+
+def capture(loc):
+	def save():
+		image.save(os.path.join(loc,f'Capture {"{:%d-%m-%Y %H_%M_%S}".format(datetime.now())}.png'))
+	global cap,transp
+	cap.attributes('-alpha',0.0)
+	image=ImageGrab.grab()
+	cap.attributes('-alpha',transp.get())
+	Thread(target=save).start()
+
+def on_close_cap(): #Edited v1.1
+	global cap
+	cap.destroy()
+	launch_button_change(0)
+
+def on_close_main(): #Added v1.1
+	global root,save_loc,transp
+	with open(os.path.join(wd,'pref.txt'),'w') as file:
+		file.truncate(0)
+		file.write(f'{save_loc.get()}\n{transp.get()}')
+		file.close()
+	root.destroy()
+
+def launch():
+	global save_loc,cap,transp
+	if save_loc.get():
+		launch_button_change(1)
+		cap=Toplevel(root)
+		cap.title('')
+		if transp.get()=='':
+			transp=0.1
+		cap.attributes('-alpha',transp.get())
+		cap.overrideredirect(True)
+		cap.attributes('-topmost', True)
+		cap.protocol("WM_DELETE_WINDOW",on_close_cap) #Edited v1.1
+		click=Button(cap,image=logo,command=lambda:capture(save_loc.get()))
+		click.pack(ipadx=0,ipady=0)
+		screen_width=root.winfo_screenwidth()
+		screen_height=root.winfo_screenheight()
+		x=screen_width-click.winfo_reqwidth()
+		y=screen_height-click.winfo_reqheight()
+		cap.geometry('%dx%d+%d+%d'%(click.winfo_reqwidth(),click.winfo_reqheight(),x,y))
+	else:
+		messagebox.showerror('Error','Save Location not provided')
+
+def getSaveLocation():
+	global save_loc
+	loc=filedialog.askdirectory()
+	if loc!='':
+		save_loc.set(loc)
+
+#GUI
+root=Tk()
+root.title('CaptureA')
+root.minsize(300,400)
+root.resizable(False,False)
+root.iconphoto(False,PhotoImage(file=resource_path('CaptureA_logo.png')))
+root.configure(bg='grey10')
+root.protocol("WM_DELETE_WINDOW",on_close_main) #Added v1.1
+
+logo=Image.open(resource_path('CaptureA_logo.png'))
+logo_disp=logo.resize((100, 100), Image.ANTIALIAS)
+logo_disp=ImageTk.PhotoImage(logo_disp)
+logo=logo.resize((50, 50), Image.ANTIALIAS)
+logo=ImageTk.PhotoImage(logo)	
+
+head=Label(root,text='CaptureA',bg='grey10',fg='white',font=('',20)).pack(padx=10,pady=10)
+
+cpyrt_label=Label(root,text='Copyright © 2020 Aditya Singh Tejas',bg='grey35',fg='white')
+cpyrt_label.pack(side='bottom',fill='x')
+launch_button=Button(root,fg='white',font=('Sans',15))
+launch_button.pack(side='bottom',padx=10,pady=10,fill='x')
+inst='Click the botton at the bottom of your screen to capture screenshot'
+inst_label=Label(root,bg='grey10',fg='white')
+inst_label.pack(fill='x',side='bottom')
+launch_button_change(0)
+logo_label=Label(root,image=logo_disp,bg='grey10')
+logo_label.pack(fill='both',side='bottom',pady=20)
+
+
+label_frame=Frame(root,bg='grey10')
+label_frame.pack(side='left',fill='both',padx=10)
+disp_frame=Frame(root,bg='grey10')
+disp_frame.pack(side='left',fill='both')
+act_frame=Frame(root,bg='grey10')
+act_frame.pack(side='right',fill='both',padx=10)
+
+save_label=Label(label_frame,text='Save Location',bg='grey10',fg='white',font=('Helvetica',12)).pack(side='top',pady=10)
+save_loc=StringVar()
+loc_entry=Entry(disp_frame,textvariable=save_loc,bg='grey25',fg='white',font=('Helvetica',12),relief='flat',insertbackground='white')
+loc_entry.pack(side='top',pady=10,fill='x')
+browse_save_button=Button(act_frame,text='Browse',command=getSaveLocation,bg='black',fg='white',font=('Helvetica',10),relief='flat')
+browse_save_button.pack(side='top',pady=8,fill='x')
+
+transp_label=Label(label_frame,text='Transparency',bg='grey10',fg='white',font=('Helvetica',12)).pack(side='top',pady=(8,10))
+transp=StringVar()
+loc_entry=Entry(disp_frame,textvariable=transp,bg='grey25',fg='white',font=('Helvetica',12),relief='flat',insertbackground='white')
+loc_entry.pack(side='top',pady=10,fill='x')
+transpsc_label=Label(act_frame,text='0 - 1',bg='grey10',fg='white',font=('Helvetica',12)).pack(side='top',pady=(8,10))
+
+#Init #Added v1.1
+wd=os.path.join(os.environ['APPDATA'],'CaptureA') 
+if not os.path.exists(wd):
+    os.mkdir(wd)
+try:
+	with open(os.path.join(wd,'pref.txt'),'r') as file: 
+		data=file.read().split('\n')
+		save_loc.set(data[0])
+		if data[1]!='':
+			transp.set(data[1])
+		else:
+			transp.set('0.1')
+except:
+	with open(os.path.join(wd,'pref.txt'),'w+') as file:
+		file.write('')
+		file.close()
+	transp.set('0.1')
+
+root.mainloop()
